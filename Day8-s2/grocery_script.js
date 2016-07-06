ad6167892e47f51bf8cfa3d4f902a97548fed315
Allie Ivener <adi2hf@virginia.edu>
2016-07-06 14:55:43 +0200

    day8 session 2 grocery list

---
+++ Day8-s2/grocery_script.js
@@ -0,0 +1,38 @@
+var app = angular.module('phoneBook',[]);
+
+app.controller('PhoneCtrl', function($scope) {
+
+  $scope.newItem = "";
+  $scope.newItemQuantity = "";
+  $scope.items = [];
+
+  $scope.addItem = function() {
+    var repeat = false;
+    for(var i = 0; i < $scope.items.length; i++) {
+      if($scope.items[i].name === $scope.newItem) {
+        $scope.items[i].quantity = parseInt($scope.items[i].quantity,10) + parseInt($scope.newItemQuantity,10);
+        repeat = true;
+      }
+    }
+    if(!repeat) {
+      var item = {"name": $scope.newItem, "quantity": $scope.newItemQuantity};
+      $scope.items.push(item);
+      console.log(item);
+      $scope.newItem = "";
+      $scope.newItemQuantity = "";
+    }
+  }
+
+  $scope.incQuantity = function(item) {
+    console.log(item);
+    item.quantity = parseInt(item.quantity,10) + 1;
+  }
+
+  $scope.decQuantity = function(item) {
+    console.log(item);
+    if(parseInt(item.quantity,10) - 1 >= 0)
+      item.quantity = parseInt(item.quantity,10) - 1;
+  }
+  
+  
+});
